cda4c2e8c561f6c2bda948444e74437bef1207b0
byhow <by.how@hotmail.com>
2024-03-20 14:35:57 -0700

    feat: add some layout

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -6,6 +6,7 @@ import {
   timestamp,
   text,
   integer,
+  index
 } from "drizzle-orm/pg-core";
 
 export const curations = pgTable("curations", {
@@ -23,3 +24,17 @@ export const dump = pgTable("dump", {
   createdAt: timestamp("created_at").notNull().defaultNow(),
   eventDump: text("event_dump"),
 });
+
+export const usersTable = pgTable("users", {
+  id: varchar("id", { length: 256 }).primaryKey().notNull(),
+  username: varchar("username", { length: 256 }).notNull().unique(),
+  email: varchar("email", { length: 256 }),
+  karma: integer("karma").notNull().default(0),
+  password: varchar("password", { length: 256 }).notNull(),
+  created_at: timestamp("created_at").notNull().defaultNow(),
+  updated_at: timestamp("updated_at").notNull().defaultNow(),
+},
+  (t) => ({
+    username_idx: index("username_idx").on(t.username),
+  })
+);

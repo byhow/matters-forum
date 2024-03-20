d000d9bd70fb7180bce88abce67b7d2d87c61a8f
byhow <by.how@hotmail.com>
2024-03-20 13:39:24 -0700

    feat: stream response

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,76 @@
 import { db } from "@/lib/db";
-import { curationAbi } from "@/lib/abi";
+import {
+  CURATION_ABI,
+  erc20TokenCurationEventSignature,
+  nativeTokenCurationEventSignature,
+} from "@/lib/abi";
 import { publicOptimismClient } from "@/lib/optimism";
-import { curations, dump } from "@/lib/db/schema";
-import { count } from "drizzle-orm";
-import { getAbiItem, parseAbiItem, type Hex, decodeEventLog } from "viem";
+import { dump } from "@/lib/db/schema";
+import { parseAbiItem, type Hex, decodeEventLog } from "viem";
 
 export default async function Home() {
-  const logs = await publicOptimismClient.getLogs({
-    address: "0x5edebbdae7B5C79a69AaCF7873796bb1Ec664DB8",
-    fromBlock: BigInt(117622710),
-    toBlock: BigInt(117638279),
-  });
+  const [erc20Logs, nativeLogs] = await Promise.all([
+    publicOptimismClient.getFilterLogs({
+      filter: await publicOptimismClient.createEventFilter({
+        event: parseAbiItem(erc20TokenCurationEventSignature),
+        fromBlock: BigInt(117622710),
+        toBlock: BigInt(117638279),
+      }),
+    }),
+    publicOptimismClient.getFilterLogs({
+      filter: await publicOptimismClient.createEventFilter({
+        event: parseAbiItem(nativeTokenCurationEventSignature),
+        fromBlock: BigInt(117622710),
+        toBlock: BigInt(117638279),
+      }),
+    }),
+  ]);
 
-  const dec = logs.map((l) => {
-    decodeEventLog({
-      abi: curationAbi,
-      eventName: "Curation",
-      data: l.data,
-      topics: l.topics,
-      strict: false,
+  const dec = [...erc20Logs, ...nativeLogs].map((log) => {
+    const { args: logArgs } = decodeEventLog({
+      abi: CURATION_ABI,
+      data: log.data,
+      topics: log.topics as [signature: `0x${string}`, ...hex: Hex[]],
     });
+    const baseLog = {
+      txHash: log.transactionHash,
+      address: log.address,
+      blockNumber: Number(log.blockNumber),
+      removed: log.removed,
+    };
+    if ("curator" in logArgs) {
+      return {
+        event: {
+          curatorAddress: logArgs.curator.toLowerCase(),
+          creatorAddress: logArgs.creator.toLowerCase(),
+          uri: logArgs.uri,
+          tokenAddress: logArgs.token.toLowerCase(),
+          amount: logArgs.amount.toString(),
+        },
+        ...baseLog,
+      };
+    } else {
+      return {
+        event: {
+          curatorAddress: logArgs.from.toLowerCase(),
+          creatorAddress: logArgs.to.toLowerCase(),
+          uri: logArgs.uri,
+          tokenAddress: null,
+          amount: logArgs.amount.toString(),
+        },
+        ...baseLog,
+      };
+    }
   });
 
   publicOptimismClient.watchContractEvent({
     address: "0x5edebbdae7B5C79a69AaCF7873796bb1Ec664DB8",
-    abi: curationAbi,
+    abi: CURATION_ABI,
     eventName: "Curation",
     onLogs: (logs) => {
       const decodedLogs = logs.map((log) => {
         return decodeEventLog({
-          abi: curationAbi,
+          abi: CURATION_ABI,
           data: log.data,
           topics: log.topics as [signature: `0x${string}`, ...hex: Hex[]],
         });
@@ -49,8 +90,11 @@ export default async function Home() {
   };
   // const res = await db.select({ value: count() }).from(curations);
   return (
-    <pre>
-      Hello from supabase! Some example logs: {JSON.stringify(dec, null, 2)}
-    </pre>
+    <div>
+      <pre>
+        Hello from supabase! The node is currently: Some example logs:
+        {JSON.stringify(dec, null, 2)}
+      </pre>
+    </div>
   );
 }
---
+++ src/lib/ipfs.ts
@@ -0,0 +1,31 @@
+import { unixfs } from "@helia/unixfs";
+import { createHelia, type HeliaLibp2p } from "helia";
+import { UnixFSEntry } from "ipfs-unixfs-exporter";
+// import { Readable } from "stream";
+
+let ipfsClient: HeliaLibp2p | undefined;
+
+export const getIpfsClient = async () => {
+  if (!ipfsClient) {
+    ipfsClient = await createHelia();
+  }
+  return ipfsClient;
+};
+
+export const ipfsLs = async (client: HeliaLibp2p) => {
+  const fs = unixfs(client);
+  const subDir: UnixFSEntry[] = [];
+  for await (const entries of fs.ls(
+    `QmWEtNfgQUhYzHcvsPuTSzR23DpF3jgtpEqnphS8LUBzzM` as any
+  )) {
+    subDir.push(entries);
+  }
+  return subDir.map((s) => ({
+    cid: s.cid,
+    type: s.type,
+    path: s.path,
+    size: Number(s.size),
+    name: s.name,
+    depth: s.depth,
+  }));
+}

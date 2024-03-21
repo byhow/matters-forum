193925c3b3d29024d134567cf7316a887b95d2b4
byhow <by.how@hotmail.com>
2024-03-21 01:13:43 -0700

    feat: is it api route

--- src/app/api/rss/route.ts
+++ src/app/api/rss/route.ts
@@ -5,32 +5,32 @@ import { NextResponse } from "next/server";
 import RSS from 'rss'
 
 export async function GET(req: Request, res: NextResponse) {
-  const site_url = process.env.NEXT_BASE_URL!;
+  // const site_url = process.env.NEXT_BASE_URL!;
 
-  const feedOptions = {
-    title: "Matters RSS Feed",
-    description: "The RSS feed for all Curation @ Matters",
-    site_url,
-    feed_url: `${site_url}/rss.xml`,
-    image_url: `${site_url}/favico.ico`,
-    pubDate: new Date(),
-    copyright: `All rights reserved ${new Date().getFullYear()}`
-  }
+  // const feedOptions = {
+  //   title: "Matters RSS Feed",
+  //   description: "The RSS feed for all Curation @ Matters",
+  //   site_url,
+  //   feed_url: `${site_url}/rss.xml`,
+  //   image_url: `${site_url}/favico.ico`,
+  //   pubDate: new Date(),
+  //   copyright: `All rights reserved ${new Date().getFullYear()}`
+  // }
 
-  const feed = new RSS(feedOptions);
+  // const feed = new RSS(feedOptions);
 
-  const allCurations = (await db.select().from(curations)).map(({ blockNumber, uri, createdAt, amount }) => {
-    feed.item({
-      title: String(blockNumber),
-      url: convertIPFStoHTTPS(uri || ""),
-      date: createdAt,
-      description: `price amount is ${String(amount)}`
-    })
-  })
+  // (await db.select().from(curations)).map(({ blockNumber, uri, createdAt, amount }) => {
+  //   feed.item({
+  //     title: String(blockNumber),
+  //     url: convertIPFStoHTTPS(uri || ""),
+  //     date: createdAt,
+  //     description: `price amount is ${String(amount)}`
+  //   })
+  // })
 
-  return new Response(feed.xml({ indent: true }), {
-    headers: {
-      'Content-Type': 'application/xml; charset=utf-8',
-    },
-  })
+  // return new Response(feed.xml({ indent: true }), {
+  //   headers: {
+  //     'Content-Type': 'application/xml; charset=utf-8',
+  //   },
+  // })
 }

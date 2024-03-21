bbec3e35cea9e278f8dcbb8faf0d2754e053d8c3
byhow <by.how@hotmail.com>
2024-03-21 01:23:53 -0700

    feat: nextbasedurl

--- src/app/api/rss/route.ts
+++ src/app/api/rss/route.ts
@@ -5,33 +5,35 @@ import { NextResponse } from "next/server";
 import RSS from 'rss'
 
 export async function GET(req: Request, res: NextResponse) {
-  // const site_url = process.env.NEXT_BASE_URL!;
+  const site_url = "https://matters-forum.vercel.app";
+  if (!site_url) {
+    return NextResponse.json({ error: "unable to load base url" }, { status: 500 });
+  }
 
-  // const feedOptions = {
-  //   title: "Matters RSS Feed",
-  //   description: "The RSS feed for all Curation @ Matters",
-  //   site_url,
-  //   feed_url: `${site_url}/rss.xml`,
-  //   image_url: `${site_url}/favico.ico`,
-  //   pubDate: new Date(),
-  //   copyright: `All rights reserved ${new Date().getFullYear()}`
-  // }
+  const feedOptions = {
+    title: "Matters RSS Feed",
+    description: "The RSS feed for all Curation @ Matters",
+    site_url,
+    feed_url: `${site_url}/rss.xml`,
+    image_url: `${site_url}/favico.ico`,
+    pubDate: new Date(),
+    copyright: `All rights reserved ${new Date().getFullYear()}`
+  }
 
-  // const feed = new RSS(feedOptions);
+  const feed = new RSS(feedOptions);
 
-  // (await db.select().from(curations)).map(({ blockNumber, uri, createdAt, amount }) => {
-  //   feed.item({
-  //     title: String(blockNumber),
-  //     url: convertIPFStoHTTPS(uri || ""),
-  //     date: createdAt,
-  //     description: `price amount is ${String(amount)}`
-  //   })
-  // })
+  (await db.select().from(curations)).map(({ blockNumber, uri, createdAt, amount }) => {
+    feed.item({
+      title: String(blockNumber),
+      url: convertIPFStoHTTPS(uri || ""),
+      date: createdAt,
+      description: `price amount is ${String(amount)}`
+    })
+  })
 
-  // return new NextResponse(feed.xml({ indent: true }), {
-  //   headers: {
-  //     'Content-Type': 'application/xml; charset=utf-8',
-  //   },
-  // })
-  return new NextResponse()
+  return new NextResponse(feed.xml({ indent: true }), {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+    },
+  })
 }

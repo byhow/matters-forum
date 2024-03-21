d6148966686e570269be360b971169a5a6822642
byhow <by.how@hotmail.com>
2024-03-21 02:01:44 -0700

    feat: rollback rss

--- src/app/api/rss/route.ts
+++ src/app/api/rss/route.ts
@@ -2,7 +2,6 @@ import { db } from "@/lib/db";
 import { curations } from "@/lib/db/schema";
 import { convertIPFStoHTTPS } from "@/lib/ipfs";
 import { NextResponse } from "next/server";
-import RSS from 'rss'
 
 export async function GET(req: Request, res: NextResponse) {
   const site_url = process.env.BASE_URL;
@@ -10,30 +9,34 @@ export async function GET(req: Request, res: NextResponse) {
     return NextResponse.json({ error: "unable to load base url" }, { status: 500 });
   }
 
-  const feedOptions = {
-    title: "Matters RSS Feed",
-    description: "The RSS feed for all Curation @ Matters",
-    site_url,
-    feed_url: `${site_url}/rss.xml`,
-    image_url: `${site_url}/favico.ico`,
-    pubDate: new Date(),
-    copyright: `All rights reserved ${new Date().getFullYear()}`
-  }
+  // const feedOptions = {
+  //   title: "Matters RSS Feed",
+  //   description: "The RSS feed for all Curation @ Matters",
+  //   site_url,
+  //   feed_url: `${site_url}/rss.xml`,
+  //   image_url: `${site_url}/favico.ico`,
+  //   pubDate: new Date(),
+  //   copyright: `All rights reserved ${new Date().getFullYear()}`
+  // }
 
-  const feed = new RSS(feedOptions);
+  // const feed = new RSS(feedOptions);
 
-  (await db.select().from(curations)).map(({ blockNumber, uri, createdAt, amount }) => {
-    feed.item({
-      title: String(blockNumber),
-      url: convertIPFStoHTTPS(uri || ""),
-      date: createdAt,
-      description: `price amount is ${String(amount)}`
-    })
+  const allCurations = (await db.select().from(curations)).map(({ blockNumber, uri, createdAt, amount }) => {
+    return {
+      blockNumber, uri, createdAt, amount: Number(amount)
+    }
+    // feed.item({
+    //   title: String(blockNumber),
+    //   url: convertIPFStoHTTPS(uri || ""),
+    //   date: createdAt,
+    //   description: `price amount is ${String(amount)}`
+    // })
   })
 
-  return new NextResponse(feed.xml({ indent: true }), {
-    headers: {
-      'Content-Type': 'application/xml; charset=utf-8',
-    },
-  })
+  // return new NextResponse(feed.xml({ indent: true }), {
+  //   headers: {
+  //     'Content-Type': 'application/xml; charset=utf-8',
+  //   },
+  // })
+  return NextResponse.json({ post: allCurations }, { status: 200 })
 }

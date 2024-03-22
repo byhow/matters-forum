8a429ac1c549d0c78f30b5cbaf46569f52e173ba
byhow <by.how@hotmail.com>
2024-03-21 17:42:24 -0700

    chore: ignore ipfs route

--- src/app/api/ipfs/route.ts
+++ src/app/api/ipfs/route.ts
@@ -1,32 +1,33 @@
-import { getIpfsClient, ipfsLs } from "@/lib/ipfs";
-import { unixfs } from "@helia/unixfs";
+// import { getIpfsClient, ipfsLs } from "@/lib/ipfs";
+// import { unixfs } from "@helia/unixfs";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request, res: NextResponse) {
-  const ipfsClient = await getIpfsClient();
-  const subs = await ipfsLs(ipfsClient)
-  let cid = subs[0].cid
+  // const ipfsClient = await getIpfsClient();
+  // const subs = await ipfsLs(ipfsClient)
+  // let cid = subs[0].cid
 
-  if (subs.length > 1) {
-    for (const child of subs) {
-      if (child.name === 'index.html') {
-        cid = child.cid;
-        break;
-      }
-    }
-  }
+  // if (subs.length > 1) {
+  //   for (const child of subs) {
+  //     if (child.name === 'index.html') {
+  //       cid = child.cid;
+  //       break;
+  //     }
+  //   }
+  // }
 
-  const fs = unixfs(ipfsClient!);
-  const decoder = new TextDecoder()
-  let text = ''
-  for await (const chunk of fs.cat(cid, {
-    onProgress: (evt) => {
-      console.info('cat event', evt.type, evt.detail)
-    }
-  })) {
-    text += decoder.decode(chunk, {
-      stream: true
-    })
-  }
-  return new NextResponse(text)
+  // const fs = unixfs(ipfsClient!);
+  // const decoder = new TextDecoder()
+  // let text = ''
+  // for await (const chunk of fs.cat(cid, {
+  //   onProgress: (evt) => {
+  //     console.info('cat event', evt.type, evt.detail)
+  //   }
+  // })) {
+  //   text += decoder.decode(chunk, {
+  //     stream: true
+  //   })
+  // }
+  // return new NextResponse(text)
+  return NextResponse.json({ message: "ipfs checkpoint" }, { status: 200 })
 }
